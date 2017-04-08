458ffde9976d4f29a5c7178a05f25d476d7572fb
ChihWei <cwl.chihweil@gmail.com>
2017-04-08 17:18:54 -0600

    change port number

--- src/main/scala/edu/colorado/plv/fixr/FixrServer.scala
+++ src/main/scala/edu/colorado/plv/fixr/FixrServer.scala
@@ -126,9 +126,9 @@ object FixrServer {
         }
       }
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8081)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    println(s"Server online at http://localhost:8081/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
